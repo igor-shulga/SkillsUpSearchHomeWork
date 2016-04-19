b9f85080c251ee2ed7ac6ebe2606d338b6beaa32
SashaPustova1988 <Shurenochek_zi@mail.ru>
2016-04-19 19:49:07 +0300

    upd

--- src/main/java/SkillsUpSearchTest.java
+++ src/main/java/SkillsUpSearchTest.java
@@ -1,16 +1,13 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class SkillsUpSearchTest extends BasePage {
     private By skillsUpTeam = new By.ByClassName("name");// or "member" if data with position needed
-    private By xPathForAlexander = new By.ByXPath("//*[@id='page_position_content']/div[3]/div[1]/div[2]/a/span[1]");
-    private boolean x;
-    private By artemXpath = By.xpath("//img[contains(@src,'/media/22116/Artem-Karpov_P.jpg')]");
 
     @Test
     public void testTestLookingForArtem() throws Exception {
@@ -32,32 +29,12 @@ public class SkillsUpSearchTest extends BasePage {
 
     @Test
     public void testTestLookingForAlexander() throws Exception {
-
-        System.out.println("Start looking for Alexander Galkovski");
-        Boolean isPresent = driver.findElements(xPathForAlexander).size() > 0;
-        assertTrue("Where is Alexander?He is not found", isPresent == true);
-        System.out.println("Alexander Galkovski found");
+        String PersonName3 = "Александр Галковский";
+        System.out.println("Start looking for " + PersonName3 );
+        assertTrue("Where is Alexander?He is not found", getCoachList().contains(PersonName3));
+        System.out.println(PersonName3+" found");
     }
 
-    @Test
-    public void testFindArtemWithException() throws Exception {
-
-        artemAreYouHere();
-        assertFalse("Artem found", x == true);
-
-    }
-
-    public boolean artemAreYouHere() {
-
-        try {
-            driver.findElement(artemXpath);
-            return x = true;
-        } catch (NoSuchElementException e) {
-            System.err.println("Such element not found: " + e.getMessage());
-            return x = false;
-        }
-
-    }
 
     private List <String> getCoachList() {
         List coachCards = new ArrayList();
